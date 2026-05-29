1897db51ef9f0ba9cdd44ef1cfe1edad870c526b
sunehachoudhary <sunehachoudhary47@gmail.com>
2026-05-29 14:54:36 +0530

    integrated mongodb database connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
+const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 
+connectDB();
 const app = express();
 
 app.use(cors());
